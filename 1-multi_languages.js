b87a355c70572cc21ad7b267c61efe47239a4ef7
salomonmmunyaneza <munyanezajoel3@gmail.com>
2025-11-16 15:03:06 +0200

    1st assignment

---
+++ 1-multi_languages.js
@@ -0,0 +1,8 @@
+// 3 lines of multilanguages
+let line1 = "C is fun";
+let line2 = "Python is cool";
+let line3 = "JavaScript is amazing"
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
