3558f1e22d5ed9ac2ed58aba9c0a9ad5eac1357c
salomonmmunyaneza <munyanezajoel3@gmail.com>
2025-11-16 22:02:10 +0200

    Implement 6-multi_languages_loop.js: print three languages using array and loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,1 +1,9 @@
+const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let output = '';
+
+for (let i = 0; i < langs.length; i++) {
+    output += `${langs[i]}\n`;
+}
+
+console.log(output.slice(0, -1));
 
